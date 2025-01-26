3c84cc562f5a57c096fd8f9f09206f3c4716c15d
qinjinling <qinjinling@outlook.com>
2025-01-26 13:52:58 +0800

    tailwind version

--- src/views/Gantt1.vue
+++ src/views/Gantt1.vue
@@ -1,60 +1,62 @@
 <!-- https://phuoc.ng/collection/css-grid/create-a-gantt-chart/ -->
 <template>
-  <div class="gantt">
-    <div class="gantt__lines">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+  <div class="relative border border-slate-200 w-full">
+    <!-- Background lines -->
+    <div class="absolute inset-0 grid grid-cols-[5rem_repeat(5,1fr)] -z-10">
+      <div class="border-r border-slate-200 h-full"></div>
+      <div class="border-r border-slate-200 h-full"></div>
+      <div class="border-r border-slate-200 h-full"></div>
+      <div class="border-r border-slate-200 h-full"></div>
+      <div class="border-r border-slate-200 h-full"></div>
     </div>
 
     <!-- Header -->
-    <div class="gantt__period">
-        <div></div>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
+    <div class="relative grid grid-cols-[5rem_repeat(5,1fr)] border-b border-slate-200">
+      <div></div>
+      <div class="text-center">Mon</div>
+      <div class="text-center">Tue</div>
+      <div class="text-center">Wed</div>
+      <div class="text-center">Thu</div>
+      <div class="text-center">Fri</div>
     </div>
 
-    <div class="gantt__row">
-        <div class="gantt__assign">John</div>
-        <div class="gantt__tasks">
-            <div class="gantt__task gantt__task--done" style="grid-column: 2 / 5"></div>
-            <div class="gantt__task gantt__task--overdue" style="grid-column: 1 / 3"></div>
-        </div>
+    <!-- Rows -->
+    <div class="grid grid-cols-[5rem_1fr] border-b border-slate-200">
+      <div class="px-1">John</div>
+      <div class="grid grid-cols-5">
+        <div class="col-start-2 col-span-3 h-4 bg-green-500 rounded-lg m-1"></div>
+        <div class="col-start-1 col-span-2 h-4 bg-red-500 rounded-lg m-1"></div>
+      </div>
     </div>
 
-    <div class="gantt__row">
-        <div class="gantt__assign">Smith</div>
-        <div class="gantt__tasks">
-            <div class="gantt__task gantt__task--done" style="grid-column: 2 / 4"></div>
-            <div class="gantt__task gantt__task--todo" style="grid-column: 3 / 5"></div>
-            <div class="gantt__task gantt__task--todo" style="grid-column: 3 / 6"></div>
-        </div>
+    <div class="grid grid-cols-[5rem_1fr] border-b border-slate-200">
+      <div class="px-1">Smith</div>
+      <div class="grid grid-cols-5">
+        <div class="col-start-2 col-span-2 h-4 bg-green-500 rounded-lg m-1"></div>
+        <div class="col-start-3 col-span-2 h-4 bg-gray-300 rounded-lg m-1"></div>
+        <div class="col-start-3 col-span-3 h-4 bg-gray-300 rounded-lg m-1"></div>
+      </div>
     </div>
 
-    <div class="gantt__row">
-        <div class="gantt__assign">Foo</div>
-        <div class="gantt__tasks">
-            <div class="gantt__task gantt__task--overdue" style="grid-column: 1 / 2"></div>
-            <div class="gantt__task gantt__task--done" style="grid-column: 3 / 5"></div>
-            <div class="gantt__task gantt__task--todo" style="grid-column: 2 / 4"></div>
-            <div class="gantt__task gantt__task--todo" style="grid-column: 2 / 5"></div>
-        </div>
+    <div class="grid grid-cols-[5rem_1fr] border-b border-slate-200">
+      <div class="px-1">Foo</div>
+      <div class="grid grid-cols-5">
+        <div class="col-start-1 col-span-1 h-4 bg-red-500 rounded-lg m-1"></div>
+        <div class="col-start-3 col-span-2 h-4 bg-green-500 rounded-lg m-1"></div>
+        <div class="col-start-2 col-span-2 h-4 bg-gray-300 rounded-lg m-1"></div>
+        <div class="col-start-2 col-span-3 h-4 bg-gray-300 rounded-lg m-1"></div>
+      </div>
     </div>
 
-    <div class="gantt__row">
-        <div class="gantt__assign">Bar</div>
-        <div class="gantt__tasks">
-            <div class="gantt__task gantt__task--overdue" style="grid-column: 1 / 4"></div>
-            <div class="gantt__task gantt__task--done" style="grid-column: 2 / 4"></div>
-            <div class="gantt__task gantt__task--todo" style="grid-column: 3 / 5"></div>
-        </div>
+    <div class="grid grid-cols-[5rem_1fr] border-b border-slate-200">
+      <div class="px-1">Bar</div>
+      <div class="grid grid-cols-5">
+        <div class="col-start-1 col-span-3 h-4 bg-red-500 rounded-lg m-1"></div>
+        <div class="col-start-2 col-span-2 h-4 bg-green-500 rounded-lg m-1"></div>
+        <div class="col-start-3 col-span-2 h-4 bg-gray-300 rounded-lg m-1"></div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script setup>
@@ -62,61 +64,4 @@
 </script>
 
 <style scoped>
-.gantt {
-  border: 1px solid rgb(203 213 225);
-  position: relative;
-  width: 100%;
-}
-.gantt__lines {
-  position: absolute;
-  left: 0;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  z-index: -1;
-
-  display: grid;
-  grid-template-columns: 5rem repeat(5, 1fr);
-  grid-template-rows: 1fr;
-}
-.gantt__lines div {
-    border-right: 1px solid rgb(203 213 225 / 0.4);
-}
-.gantt__period {
-  display: grid;
-  grid-template-columns: 5rem repeat(5, 1fr);
-}
-.gantt__period div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.gantt__row {
-  display: grid;
-  grid-template-columns: 5rem 1fr;
-  align-items: center;
-  border-top: 1px solid rgb(203 213 225);
-  padding: 0.5rem 0;
-}
-.gantt__assign {
-  padding: 0 0.25rem;
-}
-.gantt__tasks {
-  display: grid;
-  grid-row-gap: 0.25rem;
-  grid-template-columns: repeat(5, 1fr);
-}
-.gantt__task {
-    border-radius: 9999px;
-    height: 1rem;
-}
-.gantt__task--done {
-    background: rgb(74 222 128);
-}
-.gantt__task--todo {
-    background: rgb(96 165 250);
-}
-.gantt__task--overdue {
-    background: rgb(190 18 60);
-}
 </style>
